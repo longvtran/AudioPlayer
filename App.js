aa2d8fabf571b009f96d1f576e7d6613ae1fc69b
Long Tran <long.tran92@gmail.com>
2020-08-29 22:10:36 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+
+import { createStackNavigator } from "react-navigation-stack";
+
+import LandingScreen from "./app/screens/LandingScreen";
+import PlaylistScreen from "./app/screens/PlaylistScreen";
+
+const AppNavigator = createStackNavigator(
+  {
+    Landing: {
+      screen: LandingScreen
+    },
+    Playlist: {
+      screen: PlaylistScreen
+    },
+  },
+  { initialRouteName: "Landing" }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default function App() {
+  return <AppContainer />;
+}
